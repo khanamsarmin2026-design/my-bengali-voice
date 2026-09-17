316cd2d2c2ab1627b2f8e075846893a92fdd860f
khanamsarmin2026-design <khanamsarmin2026@gmail.com>
2026-09-18 00:26:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/index.html");
+});
 
 const PORT = process.env.PORT || 3000;
 const VOICE_ID = process.env.CARTESIA_VOICE_ID || "983b8905-adfb-4577-a484-da132443bdfa";
